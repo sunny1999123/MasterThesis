feb87fffb6c30f943be0f744835ff8de18475f86
sunny1999123 <590913ab@eur.nl>
2023-03-23 09:24:18 +0100

    Checking results

--- 3.Cleaning Results.R
+++ 3.Cleaning Results.R
@@ -6,3 +6,27 @@ library(XBRL)
 
 #Read results 
 Results <- read.csv("result_df.csv")
+
+
+Assets <- grepl("Assets", Results$desc, ignore.case = TRUE)
+
+Assets <- Results[Assets, ]
+
+Cash <- grepl("Cash", Results$desc, ignore.case = TRUE)
+
+Cash <- Results[Cash, ]
+
+Receivable <- grepl("Receivable", Results$desc, ignore.case = TRUE)
+
+Receivable <- Results[Receivable, ]
+Inventory <- grepl("Inventory", Results$desc, ignore.case = TRUE)
+
+Inventory <- Results[Inventory, ]
+ResearchAndDevelopmentExpense <- grepl("ResearchAndDevelopmentExpense", Results$desc, ignore.case = TRUE)
+
+ResearchAndDevelopmentExpense <- Results[ResearchAndDevelopmentExpense, ]
+
+
+Earnings <- grepl("Earnings", Results$desc, ignore.case = TRUE)
+
+Earnings <- Results[Earnings, ]
