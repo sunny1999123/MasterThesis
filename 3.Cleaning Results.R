2b6841cc19776d1e65c5767fcd7100b39fb22c8d
sunny1999123 <590913ab@eur.nl>
2023-03-27 12:13:49 +0200

    Deleting columns which are not needed and start of analysis of columns

--- 3.Cleaning Results.R
+++ 3.Cleaning Results.R
@@ -6,12 +6,30 @@ library(XBRL)
 
 #Read results of financial info
 Results <- read.csv("result_df.csv")
+DF_results <- subset(Results, select = c("val", "fy","desc", "symbol"))
+
 
 #Clean results 
 #First only keep the interesting columns, and then change from long to wide format
 #Some variables are stored with different names, every variable needs to be evaluated. 
-Assets <- grepl("Assets", Results$desc, ignore.case = TRUE)
 
-Assets <- Results[Assets, ]
+#Check the sum of the number of unique firms per year , which should equal the number of firms per item
+DF_results$FY_symbol <- paste(DF_results$fy, DF_results$symbol, sep=" ")
+NumberFirms <- unique(DF_results$FY_symbol)
+length(NumberFirms) #answer is 2602
+#So each item used should occur 2602 times 
+
+
+#Revenue
+Revenue <- grepl("revenue", DF_results$desc, ignore.case = TRUE)
+Revenue <- DF_results[Revenue,]
+
+desc_counts_rev <- Revenue %>%
+  group_by(desc) %>%
+  summarise(count = n())%>%
+  arrange(desc(count))
+head(desc_counts_rev,10)
+
+
 
 
