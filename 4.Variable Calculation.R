b63d71ba23c451128fe44f55ddeeee0b4b09a54a
sunny1999123 <590913ab@eur.nl>
2023-04-10 13:39:57 +0200

    Load Libraries and winsorize

--- 4.Variable Calculation.R
+++ 4.Variable Calculation.R
@@ -3,11 +3,24 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-
+library(scales)
+library(robustHD)
 #Load dataframe
-Results <- read.csv("CleanedResults.csv")
-
+Results <- as.data.frame(read.csv("CleanedResults.csv"))
 Results <-Results %>% arrange(symbol, fy)
+
+#Winsorize
+winsorize_cols <- function(df, lower_quantile = 0.025, upper_quantile = 0.975) {
+  df_winsorized <- df
+  for (i in 4:19) {
+    df_winsorized[, i] <- winsorize(df[, i], probs = c(lower_quantile, upper_quantile))
+  }
+  df_winsorized
+}
+
+Results <- winsorize_cols(Results, 0.05, 0.95)
+
+
 
 #Variable calculation
 Results$AccountsReceivableTurnover <- Results$Revenues/Results$AccountsReceivable
@@ -51,6 +64,41 @@ Results <- Results %>%
     ChangeInEBITDAMarginRatio = EBITDAMarginRatio - lag(EBITDAMarginRatio, default = first(EBITDAMarginRatio)),
     ChangeInDebt = debt - lag(debt, default = first(debt))
   )
+
+#Delete 2017 Rows (STILL DECIDE TO USE)
+# Results <- Results %>% 
+#   group_by(symbol) %>% 
+#   slice(-1)
+# Results <- filter(Results, fy!="2017")
+
+
+
+#Delte inf rows
+numeric_cols <- sapply(Results, is.numeric)
+Results <- Results[apply(Results[, numeric_cols], 1, function(x) all(is.finite(x))), ]
+
+
+#Rescale variables
+rescale_vars <- function(data, vars, range=c(0.1, 0.9)) {
+  # Select the specified variables
+  selected_vars <- data[, vars]
+  # Scale the variables to be between 0 and 1
+  scaled_vars <- as.data.frame(scale(selected_vars))
+  # Rescale the variables to the specified range
+  rescaled_vars <- apply(scaled_vars, 2, rescale, to=range)
+  # Combine the rescaled variables with the remaining columns
+  new_data <- cbind(data[, -vars], rescaled_vars)
+  # Return the new data frame
+  return(new_data)
+}
+final_data <- rescale_vars(Results, 20:ncol(Results), c(0.1, 0.9))
+
+#EM calculation 
+final_data$TotalCurrentAccruals <- (final_data$CurrentAssets- lag(final_data$CurrentAssets, default = first(final_data$CurrentAssets)))
+
+
+
+
 
 
 
