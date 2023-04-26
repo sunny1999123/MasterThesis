25c6e6acf534bacc02066b97d86e46da70957a38
sunny1999123 <590913ab@eur.nl>
2023-04-26 16:09:35 +0200

    Load data

--- 5.LogisticRegression.R
+++ 5.LogisticRegression.R
@@ -1,5 +1,14 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library("rpart")
+library("partykit")
+library("rpart.plot")
+library("tidymodels")
+
+
+Results <- as.data.frame(read.csv("Filtered_Results.csv"))
+Results$DiscretionaryAccrualsBinary <- as.factor(Results$DiscretionaryAccrualsBinary)
+
 
 
