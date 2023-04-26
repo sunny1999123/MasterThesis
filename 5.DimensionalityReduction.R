b885a31568c2eb7feda466255c667e0aca59c74e
sunny1999123 <590913ab@eur.nl>
2023-04-26 11:58:19 +0200

    Apply Lasso regression

--- 5.DimensionalityReduction.R
+++ 5.DimensionalityReduction.R
@@ -3,7 +3,13 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-
+library(glmnet)
+library(caret)
+library("tidymodels")
+library("glmnet")
+library("kernlab")
+library("skimr")
+library(recipes)
 #Load dataframe
 Results <- as.data.frame(read.csv("PreDimensionalityData"))
 
@@ -76,6 +82,107 @@ EMPlot <- ggplot(counts, aes(x = DiscretionaryAccrualsBinary, y = count, fill = 
   ylim(0, 350) 
 
 ggsave("DiscretionaryAccruals.pdf", plot = EMPlot, width = 6, height = 4, dpi = 300)
+
+###################################################################
+#Subset Selection#
+###################################################################
+
+
+# set seed for reproducibility
+set.seed(123456)
+
+#Set target variables as factor
+Results$DiscretionaryAccrualsBinary <- as.factor(Results$DiscretionaryAccrualsBinary)
+
+#create split, stratafied on target variables
+Results_split <- initial_split(Results, prop = 0.6, strata = DiscretionaryAccrualsBinary)
+
+#Create the training and test set
+Results_train <- training(Results_split)
+Results_test <- testing(Results_split)
+
+
+
+#Check if stratifying worked
+Results_test %>% count(DiscretionaryAccrualsBinary) %>% 
+  mutate(prop=n/sum(n))
+
+
+Results_train %>% count(DiscretionaryAccrualsBinary) %>% 
+  mutate(prop=n/sum(n))
+
+#Create folds for CV
+set.seed(593222)
+cv_folds <- vfold_cv(Results_train, v = 10, strata = DiscretionaryAccrualsBinary)
+cv_folds
+
+
+#Set recipe
+glmnet_recipe <-  recipe(DiscretionaryAccrualsBinary ~ ., data = Results_train) %>%
+  update_role(symbol, fy, FY_symbol, new_role = "ignored")
+
+#Set model
+lasso_logreg <- logistic_reg(penalty = tune(), mixture = 1) %>% 
+  set_engine("glmnet")
+
+#Set wf
+lasso_wf <- workflow() %>% 
+  add_recipe(glmnet_recipe) %>% 
+  add_model(lasso_logreg)
+
+#Metrics of interest
+class_metrics <- metric_set(accuracy, f_meas, kap, bal_accuracy)
+class_metrics
+
+#Set grid for cv validation
+grid_lasso <- tibble(penalty = 10^(seq(from = -4.5, to = -0.5, length.out = 100)))
+
+#Tuning
+lasso_tune <- lasso_wf %>% 
+  tune_grid(resamples = cv_folds, 
+            grid = grid_lasso,
+            metrics = class_metrics)
+
+
+#collect metrics 
+lasso_tune_metrics <- lasso_tune %>% 
+  collect_metrics()
+lasso_tune_metrics %>% filter(.metric == "accuracy") %>% 
+  ggplot(aes(x = penalty, y = mean, 
+             ymin = mean - std_err, ymax = mean + std_err)) + 
+  geom_errorbar(alpha = 0.5) + 
+  geom_point() + 
+  scale_x_log10() + 
+  labs(y = "Accuracy", x = expression(lambda))
+
+#Select best model based on one standard error rule
+lasso_1se_model <- lasso_tune %>% 
+  select_by_one_std_err(metric = "accuracy", desc(penalty))
+lasso_1se_model
+
+
+#Finalize workflow by selecting the best model
+lasso_wf_tuned <- 
+  lasso_wf %>% 
+  finalize_workflow(lasso_1se_model)
+lasso_wf_tuned
+
+#apply model 
+lasso_last_fit <- lasso_wf_tuned %>% 
+  last_fit(Results_split, metrics = class_metrics)
+
+#Collect metrics of the model
+lasso_test_metrics <- lasso_last_fit %>% collect_metrics()
+lasso_test_metrics
+
+#Set metrics
+lasso_test_metrics <- lasso_test_metrics %>% 
+  select(-.estimator, -.config) %>% 
+  mutate(model = "lasso")
+
+#Save features of interest 
+Features <- lasso_last_fit %>% extract_fit_parsnip() %>% 
+  tidy() %>% arrange(desc(abs(estimate)))
 
 
 
