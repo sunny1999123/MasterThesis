6ef1d0048e5f80b49cb783624e3fc79bfcf83d5d
sunny1999123 <590913ab@eur.nl>
2023-04-29 17:28:16 +0200

    SVM code

--- 8.SupportVectorMachine.R
+++ 8.SupportVectorMachine.R
@@ -14,6 +14,7 @@ library("ggridges")
 library("themis")
 library("knitr")
 library("xgboost")
+library("e1071")
 
 
 Results <- as.data.frame(read.csv("Filtered_Results.csv"))
@@ -21,3 +22,42 @@ Results$DiscretionaryAccrualsBinary <- as.factor(Results$DiscretionaryAccrualsBi
 Results$DiscretionaryAccrualsBinary <- factor(Results$DiscretionaryAccrualsBinary, levels = c("1", "0"))
 str(Results$DiscretionaryAccrualsBinary)
 
+
+set.seed(912340)
+Results_split <- initial_split(data = Results, prop = 0.7, 
+                               strata = DiscretionaryAccrualsBinary)
+
+
+Results_train <- training(Results_split)
+Results_test <- testing(Results_split)
+
+set.seed(12345678)
+cv_folds <- Results_train %>% vfold_cv(v = 10, strata = DiscretionaryAccrualsBinary)
+
+
+
+
+
+svm_recipe <- recipe(DiscretionaryAccrualsBinary ~ ., data = Results_train) %>%
+  update_role(symbol, fy, FY_symbol, new_role = "ignored") %>%
+  step_downsample(DiscretionaryAccrualsBinary) 
+
+svm_model_tune <- svm(cost = tune(), gamma = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("e1071")
+
+
+
+svm_tune_wf <- workflow() %>%
+  add_recipe(svm_recipe) %>%
+  add_model(svm_model_tune)
+
+class_metrics <- metric_set(accuracy, sensitivity, specificity, roc_auc)
+
+
+svm_tune_res <- tune_grid(
+  svm_tune_wf,
+  resamples = cv_folds,
+  grid = expand.grid(cost = c(0.1, 1, 10), gamma = c(0.01, 0.1, 1)),
+  metrics = class_metrics
+)
