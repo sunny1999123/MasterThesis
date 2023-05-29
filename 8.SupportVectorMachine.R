5d31edcf5e1b9827a316976793146352d5a7aed7
sunny1999123 <590913ab@eur.nl>
2023-05-29 17:39:16 +0200

    Change tuning figure

--- 8.SupportVectorMachine.R
+++ 8.SupportVectorMachine.R
@@ -104,7 +104,7 @@ svm_sens_spec <- svm_tune_res %>%
   ggplot(aes(x = cost, y = mean, 
              colour = .metric)) +
   geom_path() +
-  facet_wrap(~ paste("λ =", rbf_sigma)) +
+  facet_wrap(~ paste("Lamda =", rbf_sigma)) +
   labs(x = "Cost",y = "Metric Value", color = "Metrics:") +
   scale_color_manual(values=c("black", "blue", "green", "purple")) 
 
